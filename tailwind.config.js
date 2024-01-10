ed6265ef20d4e0aebe5d49700d5f9df3fc028288
Asaromi <a.sabililromi@gmail.com>
2024-01-10 12:31:27 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      borderRadius: {
+        1: '1px',
+        2: '2px',
+      },
+      height: {
+        fhd: '1080px',
+        '34.9px': '34.9px',
+        '62px': '62px',
+        '65px': '65px',
+        '73.45px': '73.45px',
+      },
+      screens: {
+        base: '320px',
+        xs: '420px',
+      },
+      spacing: {
+        '1.25': '0.3125rem',
+        '2.25': '0.5625rem',
+        '4.5': '1.125rem',
+      },
+      width: {
+        fhd: '1920px',
+        '34.9px': '34.9px',
+        '62px': '62px',
+        '65px': '65px',
+        '73.45px': '73.45px',
+      },
+    },
+    maxHeight: (theme) => ({
+      ...theme('height'),
+    }),
+    maxWidth: (theme) => ({
+      ...theme('width'),
+    }),
+    minHeight: (theme) => ({
+      ...theme('height'),
+    }),
+    minWidth: (theme) => ({
+      ...theme('width'),
+    }),
+  },
+  plugins: [],
+}
+
